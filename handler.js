056ef5ec31f478209601e6873921387d4e904fb3
Leon Matias Roslin <leonmatias1991@gmail.com>
2020-10-18 00:12:22 -0500

    .

--- handler.js
+++ handler.js
@@ -1,18 +1,26 @@
 'use strict';
+const verifyPasswordLength = require('./constraints/verifyPasswordLength');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.password = async event => {
+    try {
+        const {password} = event.pathParameters;
+        await verifyPasswordLength(password);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `todo ok ${password}`
+            })
+        }
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: `Error ${e.message}`
+            })
+        }
+    }
 };
+
+// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
